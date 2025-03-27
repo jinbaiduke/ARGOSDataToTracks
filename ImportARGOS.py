3aee76d477bae786ad93e7a916bdee8aac554c10
jinbaiduke <jin.bai@duke.edu>
2025-03-27 10:03:22 -0400

    Create point object and handle errors

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -47,6 +47,9 @@ while lineString:
         
         # Extract attributes from the datum header line
         tagID = lineData[0]
+        obsDate = lineData[3]
+        obsTime = lineData[4]
+        obsLC = lineData[7]
         
         # Extract location info from the next line
         line2String = inputFileObj.readline()
@@ -58,8 +61,32 @@ while lineString:
         obsLat = line2Data[2]
         obsLon= line2Data[5]
         
-        # Print results to see how we're doing
-        print (tagID,"Lat:"+obsLat,"Long:"+obsLon)
+        #Try some code:
+        try:
+        
+            # Print results to see how we're doing
+            print (tagID,"Lat:"+obsLat,"Long:"+obsLon,
+                "Date:"+obsDate,"Time:"+obsTime,"LC:"+obsLC)
+            
+            # Convert raw coordinate strings to numbers
+            if obsLat[-1] == 'N':
+                obsLat = float(obsLat[:-1])
+            else:
+                obsLat = float(obsLat[:-1]) * -1
+            if obsLon[-1] == 'E':
+                obsLon = float(obsLon[:-1])
+            else:
+                obsLon = float(obsLon[:-1]) * -1
+            
+            
+            # Construct a point object from the feature class
+            obsPoint = arcpy.Point()
+            obsPoint.X = obsLon
+            obsPoint.Y = obsLat
+        
+        #Handle any error
+        except Exception as e:
+            print(f"Error adding record {tagID} to the output: {e}")
         
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
